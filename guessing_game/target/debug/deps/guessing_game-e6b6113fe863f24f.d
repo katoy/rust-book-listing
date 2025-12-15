06288e2893e39297ffa8b6f7681954c04681f8f5
katoy <youichikato@gmail.com>
2025-12-16 01:58:02 +0900

    first commit

---
+++ guessing_game/target/debug/deps/guessing_game-e6b6113fe863f24f.d
@@ -0,0 +1,5 @@
+/Users/katoy/github/study-rust/book-listing/guessing_game/target/debug/deps/guessing_game-e6b6113fe863f24f.d: src/main.rs
+
+/Users/katoy/github/study-rust/book-listing/guessing_game/target/debug/deps/libguessing_game-e6b6113fe863f24f.rmeta: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/katoy/github/study-rust/book-listing/guessing_game/target/debug/guessing_game: /Users/katoy/github/study-rust/book-listing/guessing_game/src/main.rs
