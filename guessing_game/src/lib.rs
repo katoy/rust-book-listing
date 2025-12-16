bf969fa30c00e2605233e21d869a8c71568bb5c5
katoy <youichikato@gmail.com>
2025-12-16 18:36:50 +0900

    対応を大阪弁にした

--- guessing_game/src/lib.rs
+++ guessing_game/src/lib.rs
@@ -6,16 +6,16 @@ use std::io::{self, BufRead, Write};
 /// # Errors
 /// 入出力操作が失敗した場合に `io::Error` を返す
 pub fn run_game<R: BufRead, W: Write>(reader: &mut R, writer: &mut W) -> io::Result<()> {
-    writeln!(writer, "Guess the number!")?; // 数を当ててごらん
-    writeln!(writer, "Please input your guess.")?; // ほら、予想を入力してね
+    writeln!(writer, "数当ててみぃや！")?; // Guess the number!
+    writeln!(writer, "ほな、予想入れてみて！")?; // Please input your guess.
 
     let mut guess = String::new();
 
     reader.read_line(&mut guess)?; // 行の読み込み
 
     match parse_guess(&guess) {
-        Some(num) => writeln!(writer, "You guessed: {}", num)?, // 次のように予想しました: {}
-        None => writeln!(writer, "Please enter a valid number!")?, // 有効な数値を入力してください
+        Some(num) => writeln!(writer, "あんたの予想は {} やな！", num)?, // You guessed: {}
+        None => writeln!(writer, "ちゃんとした数字入れてや！")?, // Please enter a valid number!
     }
 
     Ok(())
